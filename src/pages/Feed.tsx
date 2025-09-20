465b8b603e56416b9d2a13fb0e241b0904b2d9e6
Papp Dávid <impappdavid@gmail.com>
2025-09-20 17:08:03 +0200

    Add Feed page with Navbar and FeedLayout
    Introduces a new Feed page that renders the Navbar and FeedLayout components. This sets up the basic structure for the feed section of the application.

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,12 @@
+import FeedLayout from "@/components/mycomponents/feed/layout"
+import Navbar from "@/components/mycomponents/navbar"
+
+function Feed() {
+    return (
+        <>
+            <Navbar />
+            <FeedLayout />
+        </>
+    )
+}
+export default Feed
