75c77ed65f4d1c0e285d156998c44c2eb3b9cbf1
Papp Dávid <impappdavid@gmail.com>
2025-09-20 17:07:30 +0200

    Add theme provider and UI components setup
    Introduced a ThemeProvider for dark/light/system themes and added initial UI components (dropdown menu, input, slider). Updated App.tsx to use React Router and the new theme provider. Also updated project metadata and dependencies to include Radix UI and react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feed from './pages/Feed';
+import { ThemeProvider } from "@/components/mycomponents/theme-provider";
 
 function App() {
 
   return (
     <>
-      <div>
-        <div className="">Revalo</div>
-      </div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              <Route index element={`Hello World`} />
+              <Route path="feed" element={<Feed />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
