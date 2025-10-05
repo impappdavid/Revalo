4144028b249f6f7218467e3f1eec706076c74618
Papp Dávid <impappdavid@gmail.com>
2025-10-05 20:01:00 +0200

    Add Clips page with layout and navbar
    Introduces a new Clips page that renders the Navbar and ClipsLayout components. This sets up the basic structure for the Clips section of the application.

---
+++ src/pages/Clips.tsx
@@ -0,0 +1,12 @@
+import ClipsLayout from "@/components/mycomponents/clips/layout"
+import Navbar from "@/components/mycomponents/navbar"
+
+function Clips() {
+    return (
+        <>
+            <Navbar />
+            <ClipsLayout />
+        </>
+    )
+}
+export default Clips
