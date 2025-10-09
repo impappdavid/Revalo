d95b8c5dba543cb314ffa9e9da4375c58593c2a2
Papp Dávid <impappdavid@gmail.com>
2025-10-09 14:30:43 +0200

    Types for the jobs

---
+++ src/components/mycomponents/jobs/types.ts
@@ -0,0 +1,12 @@
+// types.ts
+export interface Job {
+    id: number;
+    title: string;
+    company: string;
+    location: string;
+    logo: string;
+    skills: string[];
+    description: string;
+    role: string;
+    posted: string;
+}
