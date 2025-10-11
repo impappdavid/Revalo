c1b4d3fcfdcc71353f4f258175355e3f9833d578
Papp Dávid <impappdavid@gmail.com>
2025-10-11 16:54:32 +0200

    Making the market components responsive

--- src/components/mycomponents/market/topSellers.tsx
+++ src/components/mycomponents/market/topSellers.tsx
@@ -32,7 +32,7 @@ function TopSellers() {
                     <div className="text-xs text-zinc-400 px-3 py-1.5 hover:bg-zinc-800/80 cursor-pointer transition-all duration-200 rounded-md hover:text-white">View All</div>
                 </div>
 
-                <div className="grid grid-cols-5 gap-3">
+                <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3">
                     {data.map((item, index) => (
                         <div key={index} className="flex flex-col gap-1 cursor-pointer">
                             <img src={item.imageSource}
