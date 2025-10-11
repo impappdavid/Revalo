d612ffee426efae82060db64d8c3046168ad5254
Papp Dávid <impappdavid@gmail.com>
2025-10-11 12:28:18 +0200

    Making the market page layout

---
+++ src/components/mycomponents/market/layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "../sidebar"
+import Menu from "./menu"
+
+function MarketLayout() {
+    return (
+        <>
+            <div className="w-full flex justify-center dark:bg-zinc-950 bg-zinc-100 min-h-screen">
+                <div className="w-full max-w-6xl flex p-2 gap-4">
+                    <Sidebar />
+                    <div className="w-full hidden sm:flex max-w-40"></div>
+                    <div className="flex flex-col w-full relative gap-4">
+                        <Menu />
+                    </div>
+
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+export default MarketLayout
